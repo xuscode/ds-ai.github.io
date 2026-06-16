1bcfe9a374b366b722f544f5f1140ddc7486ff89
xuscode <song_hsu@163.com>
2026-06-16 15:19:06 +0800

    06/16/2026 Tue 15:19:06.77

---
+++ git-auto-commit-src.bat
@@ -0,0 +1,10 @@
+@echo off
+REM ==============================================================================
+REM Tool: Git Upload Source
+REM Description: Adds all changes, commits, and pushes
+REM ==============================================================================
+
+cd /d "%~dp0"
+git add .
+git commit -m "%date% %time%"
+git push --set-upstream origin master
